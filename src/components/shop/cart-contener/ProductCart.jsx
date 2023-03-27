852fde633a4820e8e45177fcdd6243e6e4ba0167
rofikul-resan <resan6231@gmail.com>
2023-03-27 23:42:30 +0600

    cart component done

--- src/components/shop/cart-contener/ProductCart.jsx
+++ src/components/shop/cart-contener/ProductCart.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import "./productCart.css"
 const ProductCart = (props) => {
     const {cartData}= props;
-
+    const totalPrice  = cartData.reduce((pPrice, cPrice)=>cPrice.price + pPrice , 0);
+    const totalShipping  = cartData.reduce((pShip, cShip)=>cShip.shipping + pShip , 0);
+    const tax = (totalPrice/100)*7;
     return (
         <>
             <div>
@@ -10,6 +12,10 @@ const ProductCart = (props) => {
             </div>
             <div className="cart-info">
                 <p>Selected Items: {cartData.length} </p>
+                <p>Total Price: ${totalPrice} </p>
+                <p>Total Shipping Charge: ${totalShipping} </p>
+                <p>Tax: ${tax.toFixed(2)} </p>
+                <h5>Grand Total: ${(totalPrice+totalShipping+tax).toFixed(2)}</h5>
             </div>
         </>
     );
