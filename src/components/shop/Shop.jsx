341380e8ec8f28c94a9968470ed1b9d30300274a
rofikul-resan <resan6231@gmail.com>
2023-04-04 20:54:21 +0600

    add local storage

--- src/components/shop/Shop.jsx
+++ src/components/shop/Shop.jsx
@@ -1,20 +1,44 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb, getShoppingCart } from '../../utilities/fakedb';
 import ProductCart from './cart-contener/ProductCart';
 import ProductCard from './ProductCard/ProductCard';
 import "./Shop.css"
 
 const Shop = () => {
     const [cardsData , setCardsData ] = useState([]);
+    const [cart, setCart]= useState([]);
 
     useEffect(()=>{
         fetch("./fakeData/products.json")
         .then(res => res.json())
         .then(data => setCardsData(data))
         
-    },[])
-    const [cart, setCart]= useState([])
+    },[]);
+
+    useEffect(()=>{
+        const localDB = getShoppingCart(); 
+        const selectedProduct = [];
+        for (const id in localDB){
+            const addedProduct = cardsData.find((product) => product.id === id);
+            if(addedProduct){
+                addedProduct.quantity = localDB[id];
+                selectedProduct.push(addedProduct);
+                setCart(selectedProduct);
+            }
+        }
+    }, [cardsData])
+
     const addToCart= (product) =>{
-        setCart([...cart , product ])
+        const isEexist = cart.find(pd => pd.id === product.id);
+        if (!isEexist){
+            product.quantity = 1
+            setCart([...cart , product ])
+        } else{
+             isEexist.quantity = isEexist.quantity + 1 ;
+             const remain = cart.filter(pb => pb.id !== product.id );
+             setCart([...remain , isEexist])
+         }
+        addToDb(product.id)
     }
 
     return (
--- src/components/shop/cart-contener/ProductCart.jsx
+++ src/components/shop/cart-contener/ProductCart.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 import "./productCart.css"
 const ProductCart = (props) => {
     const {cartData}= props;
-    const totalPrice  = cartData.reduce((pPrice, cPrice)=>cPrice.price + pPrice , 0);
-    const totalShipping  = cartData.reduce((pShip, cShip)=>cShip.shipping + pShip , 0);
+    const totalQuantity  = cartData.reduce((pQuantity, cQuantity)=>cQuantity.quantity + pQuantity , 0);
+    const totalPrice  = cartData.reduce((pPrice, cPrice)=>cPrice.price * cPrice.quantity + pPrice , 0);
+    const totalShipping  = cartData.reduce((pShip, cShip)=>cShip.shipping * cShip.quantity + pShip , 0);
     const tax = (totalPrice/100)*7;
     return (
         <>
@@ -11,7 +12,7 @@ const ProductCart = (props) => {
                 <h2 className='cart-title'>Order Summary</h2>
             </div>
             <div className="cart-info">
-                <p>Selected Items: {cartData.length} </p>
+                <p>Selected Items: {totalQuantity } </p>
                 <p>Total Price: ${totalPrice} </p>
                 <p>Total Shipping Charge: ${totalShipping} </p>
                 <p>Tax: ${tax.toFixed(2)} </p>
